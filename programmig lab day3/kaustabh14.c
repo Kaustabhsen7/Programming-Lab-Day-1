05e89c9d55cd6803b2afe9bf1554b13bb9a5a557
Kaustabhsen7 <senkaustabh2006@gmail.com>
2025-08-31 11:58:28 +0530

    Update kaustabh14.c

--- programmig lab day3/kaustabh14.c
+++ programmig lab day3/kaustabh14.c
@@ -4,12 +4,18 @@
 #include<stdio.h>
 #include<math.h>
 int main() {
-	float num,f1,c1;
-	printf("enter a number :");
+	float num1,num2,f1,c1,f2,c2;
+	printf("enter a positive number :");
 	scanf("%f",&num);
-	f1=floor(num);
-	c1=ceil(num);
-	printf("floor value is :%f",f1);
-	printf("ceiling value is :%f",c1);
+	f1=floor(num1);
+	c1=ceil(num1);
+  printf("enter a negative number :");
+	scanf("%f",&num2);
+	f2=floor(num2);
+	c2=ceil(num2);
+	printf("floor value for positive number is :%f",f1);
+	printf("ceiling value for positive number is :%f",c1);
+ printf("floor value for negative number is :%f",f2);
+	printf("ceiling value for negative is :%f",c2);
 	return 0;
 }
